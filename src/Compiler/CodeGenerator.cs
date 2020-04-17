c17bdcf75424d992ac6a5f3150c3659a51f5df35
zenuas@gmail.com <zenuas@gmail.com>
2020-04-17 22:37:58 +0900

    コード生成の関数追加

---
+++ src/Compiler/CodeGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Roku.Manager;
+
+namespace Roku.Compiler
+{
+    public static class CodeGenerator
+    {
+        public static void Emit(SourceCodeBody body, string path)
+        {
+        }
+    }
+}
