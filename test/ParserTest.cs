6103a80b4582a2631776c1c36b2da954f4d785b4
zenuas@gmail.com <zenuas@gmail.com>
2020-04-26 11:07:34 +0900

    関数呼び出しパーサのテストケースを追加

--- test/ParserTest.cs
+++ test/ParserTest.cs
@@ -29,6 +29,24 @@ namespace Roku.Tests
             Assert.AreEqual(num.Value, 123456);
         }
 
+        [Test]
+        public void CallTest()
+        {
+            var p = Parse("print(\"hello world\")");
+            Assert.AreEqual(p.Statements.Count, 1);
+            Assert.IsTrue(p.Statements[0] is FunctionCallNode);
+
+            var call = p.Statements[0].Cast<FunctionCallNode>();
+            Assert.IsTrue(call.Expression is VariableNode);
+            var f = call.Expression.Cast<VariableNode>();
+            Assert.AreEqual(f.Name, "print");
+
+            Assert.AreEqual(call.Arguments.Count, 1);
+            Assert.IsTrue(call.Arguments[0] is StringNode);
+            var str = call.Arguments[0].Cast<StringNode>();
+            Assert.AreEqual(str.Value, "hello world");
+        }
+
         public static ProgramNode Parse(string s) => new Parser.Parser().Parse(LexerTest.Read(s)).Cast<ProgramNode>();
     }
 }
