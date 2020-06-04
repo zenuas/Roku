96d25dd1764b2e55f5cb35363acdfdfdee5f8e9e
zenuas@gmail.com <zenuas@gmail.com>
2020-06-04 20:44:48 +0900

    In関数移植

--- src/Extensions/Objects.cs
+++ src/Extensions/Objects.cs
@@ -22,6 +22,9 @@ namespace Extensions
         }
 
         [DebuggerHidden]
+        public static bool In<T>(this T self, params T[] args) where T : IEquatable<T> => !args.Where(self.Equals).IsNull();
+
+        [DebuggerHidden]
         public static T Cast<T>(this object self) => (T)self;
     }
 }
