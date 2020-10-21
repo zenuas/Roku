9e43579f793ab312f0d7ea60a512b970bd4ea565
zenuas@gmail.com <zenuas@gmail.com>
2020-10-21 23:16:50 +0900

    NodeIteratorを追加 ツリー状のプロパティノードをリストに変換

---
+++ src/Node/NodeIterator.cs
@@ -0,0 +1,10 @@
+﻿using Extensions;
+using System.Collections.Generic;
+
+namespace Roku.Node
+{
+    public class NodeIterator
+    {
+        public static IEnumerable<IEvaluableNode> PropertyToList(IEvaluableNode expr) => expr is PropertyNode p ? PropertyToList(p.Left).Concat(p.Right) : new List<IEvaluableNode>() { expr };
+    }
+}
