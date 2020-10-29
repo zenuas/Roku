ab9df0e69d9941b5b21d8c9cf7e078602119dcf8
zenuas@gmail.com <zenuas@gmail.com>
2020-10-29 21:48:17 +0900

    簡易構造体の名前が取得ミスしていたのを修正

--- src/Node/TypeStructNode.cs
+++ src/Node/TypeStructNode.cs
@@ -9,6 +9,6 @@ namespace Roku.Node
         public int? LineColumn { get; set; }
         public VariableNode StructName { get; set; } = new VariableNode();
         public List<DeclareNode> Arguments { get; } = new List<DeclareNode>();
-        public string Name => $"struct {StructName}({Arguments.Map(x => $"{x.Name.Name}: {x.Type.Name}").Join(", ")})";
+        public string Name => $"struct {StructName.Name}({Arguments.Map(x => $"{x.Name.Name}: {x.Type.Name}").Join(", ")})";
     }
 }
