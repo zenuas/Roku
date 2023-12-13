ad9098a933138a735b0d392a0a0b62747252fcd6
zenuas@gmail.com <zenuas@gmail.com>
2023-12-14 00:15:45 +0900

    テストケース実行をtest-compileに分離 makeをバッチに置き換え

---
+++ test-compile/Program.cs
@@ -0,0 +1,89 @@
+﻿using Command;
+using Extensions;
+using Roku.Tests;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Roku.TestCompile;
+
+public class Program
+{
+    [CommandOption('f')]
+    [CommandOption("force")]
+    public bool Force { get; init; } = false;
+
+    [CommandOption('o')]
+    [CommandOption("output")]
+    public string Output { get; init; } = Path.Combine(".", "obj");
+
+    public static void Main(string[] args)
+    {
+        var (opt, xargs) = CommandLine.Run<Program>(args);
+
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        if (Directory.Exists(opt.Output))
+        {
+            Directory.GetFiles(opt.Output, "*.*").Each(File.Delete);
+        }
+        else
+        {
+            _ = Directory.CreateDirectory(opt.Output);
+        }
+
+        var compile_result = Directory.GetFiles(".", "*.rk")
+            .MapParallelAllWithTimeout(x => FrontEndTest.Compile(x, Path.Combine(opt.Output, $"{Path.GetFileNameWithoutExtension(x)}.il"), !opt.Force), 1000 * 10, x => new(x, Path.GetFileName(x), "timeout"))
+            .ToList();
+
+        var sjis = System.Text.Encoding.GetEncoding(932);
+        foreach (var result in compile_result)
+        {
+            var testname = Path.GetFileNameWithoutExtension(result.Result.Path);
+            var in_ = Path.Combine(opt.Output, testname + ".testin");
+            var out_ = Path.Combine(opt.Output, testname + ".testout");
+            var err_ = Path.Combine(opt.Output, testname + ".testerr");
+            var args_ = Path.Combine(opt.Output, testname + ".testargs");
+            var name_ = Path.Combine(opt.Output, testname + ".testname");
+
+            File.Delete(in_);
+            File.Delete(out_);
+            File.Delete(err_);
+            File.Delete(args_);
+            File.Delete(name_);
+
+            var txt = File.ReadAllText(result.Result.Path);
+            var lines = txt.SplitLine().Select(x => x.TrimStart()).ToArray();
+            var name_p = FrontEndTest.GetLineContent(lines, x => x == "###", x => x == "###");
+            var comment = testname + (name_p.Found ? $" {name_p.Text.SplitLine().Take(2).Join(" ").SubstringAsByte(0, 78 - testname.Length, sjis)}" : "");
+
+            Console.WriteLine(comment);
+            if (opt.Force || !result.Completed || result.Result.ErrorMessage != "")
+            {
+                var in_p = lines.Where(x => x.StartsWith("#<=")).Select(x => x[3..] + "\r\n").Join();
+                var out_p = lines.Where(x => x.StartsWith("#=>")).Select(x => x[3..] + "\r\n").Join();
+                var err_p = lines.Where(x => x.StartsWith("#=2>")).Select(x => x[4..] + "\r\n").Join();
+                var args_p = lines.Where(x => x.StartsWith("##*")).Select(x => x[3..]).Join(" ");
+
+                File.WriteAllText(in_, in_p);
+                File.WriteAllText(out_, out_p);
+                File.WriteAllText(err_, err_p);
+                File.WriteAllText(args_, args_p);
+                File.WriteAllText(name_, comment);
+
+                if (!result.Completed || result.Result.ErrorMessage != "")
+                {
+                    var prev_color = Console.ForegroundColor;
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(result.Result.ErrorMessage);
+                    }
+                    finally
+                    {
+                        Console.ForegroundColor = prev_color;
+                    }
+                }
+            }
+        }
+    }
+}
--- test/FrontEndTest.cs
+++ test/FrontEndTest.cs
@@ -2,7 +2,6 @@
 using NUnit.Framework;
 using Roku.Compiler;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -11,28 +10,13 @@ namespace Roku.Tests;
 
 public class FrontEndTest
 {
-    public static string SourceDir = Path.Combine("..", "..", "..", "rk");
-    public static string ObjDir = Path.Combine("..", "..", "..", "rk", "obj");
+    public static string SourceDir = Path.Combine("..", "..", "..", "..", "test-rk");
 
     [SetUp]
     public void Setup()
     {
         Trace.Listeners.Clear();
         _ = Trace.Listeners.Add(new NoFailListener());
-    }
-
-    [OneTimeSetUp]
-    public void OneTimeSetUp()
-    {
-        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-        if (Directory.Exists(ObjDir))
-        {
-            Directory.GetFiles(ObjDir, "*.*").Each(File.Delete);
-        }
-        else
-        {
-            _ = Directory.CreateDirectory(ObjDir);
-        }
     }
 
     public static (bool Found, string Text) GetLineContent(string[] lines, Func<string, bool> start_line, Func<string, bool> end_line)
@@ -46,17 +30,16 @@ public class FrontEndTest
 
     public static (bool Found, string Text) GetLineContent(string[] lines, string start_line, string end_line) => GetLineContent(lines, x => x.StartsWith(start_line), x => x.StartsWith(end_line));
 
-    public static (string Path, string TestName, string ErrorMessage) Compile(string src, bool none_skip)
+    public static (string Path, string TestName, string ErrorMessage) Compile(string src, string il, bool il_delete)
     {
         var filename = Path.GetFileName(src);
         var txt = File.ReadAllText(src);
         var lines = txt.SplitLine();
         var testname = Path.GetFileNameWithoutExtension(src);
-        var il = Path.Combine(ObjDir, testname + ".il");
 
         try
         {
-            FrontEnd.Compile(FrontEnd.Parse(new StringReader(txt)), il, new string[] { "System.Runtime" });
+            FrontEnd.Compile(FrontEnd.Parse(new StringReader(txt)), il, ["System.Runtime"]);
 
             var valid = GetLineContent(lines, "###start", "###end");
             if (!valid.Found)
@@ -67,7 +50,7 @@ public class FrontEndTest
 
             if (valid.Text.Trim() != il_src) return (src, filename, "il make a difference");
 
-            if (!none_skip) File.Delete(il);
+            if (il_delete) File.Delete(il);
         }
         catch (Exception ex)
         {
@@ -91,57 +74,11 @@ public class FrontEndTest
     [Test]
     public void CompileTest()
     {
-        var none_skip = TestContext.Parameters.Get("none-skip", false);
         var compile_result = Directory.GetFiles(SourceDir, "*.rk")
-            .MapParallelAllWithTimeout(x => Compile(x, none_skip), 1000 * 10, x => new(x, Path.GetFileName(x), "timeout"))
+            .MapParallelAllWithTimeout(x => Compile(x, $"{Path.GetFileNameWithoutExtension(x)}.il", true), 1000 * 10, x => new(x, Path.GetFileName(x), "timeout"))
             .ToList();
 
-        var sjis = System.Text.Encoding.GetEncoding(932);
-        var success = new List<string>();
-        foreach (var result in compile_result)
-        {
-            var testname = Path.GetFileNameWithoutExtension(result.Result.Path);
-            var in_ = Path.Combine(ObjDir, testname + ".testin");
-            var out_ = Path.Combine(ObjDir, testname + ".testout");
-            var err_ = Path.Combine(ObjDir, testname + ".testerr");
-            var args_ = Path.Combine(ObjDir, testname + ".testargs");
-            var name_ = Path.Combine(ObjDir, testname + ".testname");
-
-            File.Delete(in_);
-            File.Delete(out_);
-            File.Delete(err_);
-            File.Delete(args_);
-            File.Delete(name_);
-
-            var txt = File.ReadAllText(result.Result.Path);
-            var lines = txt.SplitLine().Select(x => x.TrimStart()).ToArray();
-            var name_p = GetLineContent(lines, x => x == "###", x => x == "###");
-            var comment = testname + (name_p.Found ? $" {name_p.Text.SplitLine().Take(2).Join(" ").SubstringAsByte(0, 78 - testname.Length, sjis)}" : "");
-
-            if (!none_skip && result.Completed && result.Result!.ErrorMessage == "")
-            {
-                success.Add(comment);
-            }
-            else
-            {
-                var in_p = lines.Where(x => x.StartsWith("#<=")).Select(x => x[3..] + "\r\n").Join();
-                var out_p = lines.Where(x => x.StartsWith("#=>")).Select(x => x[3..] + "\r\n").Join();
-                var err_p = lines.Where(x => x.StartsWith("#=2>")).Select(x => x[4..] + "\r\n").Join();
-                var args_p = lines.Where(x => x.StartsWith("##*")).Select(x => x[3..]).Join(" ");
-
-                File.WriteAllText(in_, in_p);
-                File.WriteAllText(out_, out_p);
-                File.WriteAllText(err_, err_p);
-                File.WriteAllText(args_, args_p);
-                File.WriteAllText(name_, comment);
-            }
-        }
-
-        var skip = Path.Combine(ObjDir, ".success");
-        File.Delete(skip);
-        File.WriteAllText(skip, success.Count > 0 ? success.Join("\r\n") : "");
-
-        var failed = compile_result.Where(x => !x.Completed || x.Result!.ErrorMessage != "").ToList();
+        var failed = compile_result.Where(x => !x.Completed || x.Result.ErrorMessage != "").ToList();
         if (failed.Count > 0) Assert.Fail(failed.Select(x => $"{x.Result.TestName}: {x.Result.ErrorMessage}").Join("\n"));
     }
 }
